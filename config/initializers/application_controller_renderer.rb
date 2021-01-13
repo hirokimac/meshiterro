00bc3bc198469ba013e0f66da5ca822a78f92b4d
hirokimac <hiroki.yokomizo1030@gmail.com>
2021-01-14 00:12:16 +0900

    2021013_meshiterro

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'cbf2e3fdfbf10cfde848fe4dc615652fd3ae6b9ba17f5572ba6f02f75ab825b763936290923d25a56757383ea1b91993517d6e728fa6b69c4322129a2ef0b1d3'
